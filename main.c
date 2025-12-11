f20f65a6f767b51172be91c58df1bc36993e8815
annisvvv <anisnasri322@gmail.com>
2025-12-11 11:21:49 +0100

    .

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 #include <sys/wait.h>
 #include "header.h"
 
-	char command[100];
-	char* tokens[10];
-	int count, pip = 0;
+char command[100];
+char* tokens[10];
+int count, pip = 0;
+int i[10];
+char* piped_tokens[20][20];
 
 void input_token(){
 
 		pip = 0;
-		int i[10];
 		int y = 0;
 
 		//input command	
@@ -37,7 +38,7 @@ void input_token(){
 		tokens[count] = NULL;
 }
 
-void cd(char*){
+void cd(){
 
 		if (tokens[1] == NULL) {
 			chdir(getenv("HOME"));
@@ -48,6 +49,75 @@ void cd(char*){
 			}
 		}
 		//continue;   // Skip fork(), go back to loop
+}
+
+void pid(){
+
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		
+		execvp(tokens[0], tokens);
+		perror("exec failed");  
+		exit(1);
+	} else {
+		
+		wait(NULL);
+	}
+}
+
+void pippe(){
+
+	int cmd = 0;
+    int arg = 0;
+
+    for (int t = 0; tokens[t] != NULL; t++) {
+
+        if (strcmp(tokens[t], "|") == 0) {
+            piped_tokens[cmd][arg] = NULL;
+            cmd++;
+            arg = 0;
+        }
+        else {
+            piped_tokens[cmd][arg++] = tokens[t];
+        }
+    }
+
+    piped_tokens[cmd][arg] = NULL;
+
+	int cmd_count = cmd + 1;   // number of commands
+
+    // ---------------- EXECUTE PIPES -----------------
+
+    int oldfd = 0;     // input for next process
+    int fd[2];
+
+    for (int c = 0; c < cmd_count; c++) {
+
+        pipe(fd);
+        pid_t pid = fork();
+
+        if (pid == 0) {      // child
+
+            dup2(oldfd, STDIN_FILENO);   // previous pipe input
+
+            if (c < cmd_count - 1) {
+                dup2(fd[1], STDOUT_FILENO); // next pipe output
+            }
+
+            close(fd[0]); // close unused read end
+
+            execvp(piped_tokens[c][0], piped_tokens[c]);
+            perror("exec failed");
+            exit(1);
+        }
+
+        // parent
+        wait(NULL);
+        close(fd[1]);     // close write end
+        oldfd = fd[0];    // next command reads from this pipe
+    }
+
 }
 
 int main() {
@@ -63,7 +133,7 @@ int main() {
 		input_token();
 		
 		if (strcmp(tokens[0], "cd") == 0) {
-			cd(tokens[0]);
+			cd();
 		}
 		
 		else if (strcmp(tokens[0], "help") == 0){
@@ -71,18 +141,12 @@ int main() {
 			return 0;
 		}
 
-		else {
-			pid_t pid = fork();
+		else if (pip != 0){
+			pippe();
+		}
 
-			if (pid == 0) {
-				
-				execvp(tokens[0], tokens);
-				perror("exec failed");  
-				exit(1);
-			} else {
-				
-				wait(NULL);
-			}
+		else {
+			pid();
 		}
 	}
 	return 0;
